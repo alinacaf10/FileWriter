b33aa99756d1891f492dd8ac2c12b2b6206d4ab7
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-07-18 13:55:30 +0400

    Add files via upload
    Added READFile method

--- Main.java
+++ Main.java
@@ -1,30 +1,45 @@
 package Example.FWriter;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Writers writers=new Writers();
-        try {
-            Scanner sc=new Scanner(System.in);
-            Student student=new Student();
-            System.out.println("Add Name");
-            student.setName(sc.next());
-                System.out.println("Add Surname");
-            student.setSurname(sc.next());
-            System.out.println("Add Grnum (Enter only number type)");
-            student.setGrnum(sc.nextInt());
-            System.out.println("Add Lesson");
-            student.setLesson(sc.next());
-                writers.write(student);
-            System.out.println("when you need stop program you may write 'STOP' if you need continue press any key and send");
-            String stop=sc.next();
-            if (!stop.equals("STOP")) {
-                main(args);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Read or Write");
+        switch (sc.next()){
+            case "write":
+                try {        Writers writers=new Writers();
+                    Student student=new Student();
+                    System.out.println("Add Name");
+                    student.setName(sc.next());
+                    System.out.println("Add Surname");
+                    student.setSurname(sc.next());
+                    System.out.println("Add Grnum (Enter only number type)");
+                    student.setGrnum(sc.nextInt());
+                    System.out.println("Add Lesson");
+                    student.setLesson(sc.next());
+                    Writers.write(student);
+                    System.out.println("when you need stop program you may write 'STOP' if you need continue press any key and send");
+                    String stop=sc.next();
+                    if (!stop.equals("STOP")) {
+                        main(args);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "read":
+       List<String> dataList= Writers.readFile();
+        for (String data:dataList){
+            System.out.println(data);
         }
+        break;
+            default:
+                System.out.println("invalid option");
+                break;
+        }
+
     }
 }
--- Writers.java
+++ Writers.java
@@ -1,14 +1,13 @@
 package Example.FWriter;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Writers {
     public static final String Folder_Path="C:/Users/HP/Desktop";
-    public static final String File_Path=Folder_Path+File.separator+"Writerss.txt";
+    public static final String File_Path=Folder_Path+File.separator+"Student.txt";
     public static void write(Student student) throws IOException{
         File f=new File(Folder_Path) ;
         if (!f.exists()){
@@ -19,4 +18,14 @@ public class Writers {
           bw.write(student.getName()+"..."+student.getSurname()+"..."+student.getGrnum()+"..."+student.getLesson());
         bw.newLine();
     }}
+    public static List<String> readFile()throws IOException{
+        List<String> dataList = new ArrayList<>();
+        FileReader fr=new FileReader(File_Path);
+        BufferedReader br=new BufferedReader(fr);
+        while (br.ready()){
+            String data=br.readLine();
+            dataList.add(data);
+        }
+        return dataList;
+    }
 }
